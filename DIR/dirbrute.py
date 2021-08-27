ccde4475f65167890aa0b28c5e81ea7e4ad88ae8
Eagle Eye <87413459+EagleTube@users.noreply.github.com>
2021-08-27 18:27:58 +0800

    Update dirbrute.py

--- DIR/dirbrute.py
+++ DIR/dirbrute.py
@@ -1,61 +1,95 @@
-import requests
-import json
-import sys
+import cloudscraper, sys, os, colorama, time, ctypes, datetime, sys, platform, threading
 from urllib.parse import urlparse
+from colorama import Fore, Back, Style
+from datetime import date
+from time import gmtime, strftime
 
-def reqURL(target,path):
-    url = target + path
-    payload = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    # Adding empty header as parameters are being sent in payload
-    headers = {}
-    r = requests.get(url, data=json.dumps(payload), headers=headers)
-    return (r.status_code)
+today = date.today()
+d2 = today.strftime("%B %d, %Y")
 
-
-def cleanURL(target):
-    urlArr = urlparse(target)
-    if(urlArr.path==target and urlArr.scheme==""):
-        return "http://" + target + "/"
-    elif(urlArr.path==""):
-        return target + "/"
-    else:
-        return target
-
-def checkArgv(x):
-    if(x.lower()=="-w" or x.lower()=="--wordlist"):
-        return True
-    else:
-        return False
-
-if(len(sys.argv)>4):
-    print("Unknown command {}".format(sys.argv[4]))
+if platform.system()=='Linux':
+    os.system('clear')
+    sys.stdout.write("\x1b]2;SPAM-PHISHING MAIL DFM {}\x07".format(d2))
 else:
-    if checkArgv(sys.argv[2]):
-        try:
-            words = open(sys.argv[3],"r")
-            lines = words.readlines()
-            target = cleanURL(sys.argv[1])
-            for line in lines:
-                if(reqURL(target,line.strip())==200):
-                    print("Found : {}".format(target+line.strip()))
-                elif(reqURL(target,line.strip())==403):
-                    print("Forbidden : {}".format(target+line.strip()))
-                elif(reqURL(target,line.strip())==404):
-                    print("Not Found : {}".format(target+line.strip()))
-                elif(reqURL(target,line.strip())==400):
-                    print("Bad Request : {}".format(target+line.strip()))
-                elif(reqURL(target,line.strip())>=500 and reqURL(target,line.strip())<=599):
-                    print("Internal Error : {}".format(target+line.strip()))
-                elif(reqURL(target,line.strip())>=300 and reqURL(target,line.strip())<=399):
-                    print("Redirected : {}".format(target+line.strip()))
-                else:
-                    print("Unknown : {}".format(target+line.strip()))
+    os.system('cls')
+    ctypes.windll.kernel32.SetConsoleTitleW(f'SPAM-PHISHING MAIL DFM | {d2}')
 
-        except:
-            print("ERROR : Wordlist may be too large or Wordlist not existen!")
+print(f"""{Style.BRIGHT + Fore.RED}
+ ██████╗ ██████╗  █████╗  ██████╗  ██████╗ ███╗   ██╗███████╗ ██████╗ ██████╗  ██████╗███████╗   ██╗ ██████╗ 
+ ██╔══██╗██╔══██╗██╔══██╗██╔════╝ ██╔═══██╗████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝   ██║██╔═══██╗
+ ██║  ██║██████╔╝███████║██║  ███╗██║   ██║██╔██╗ ██║█████╗  ██║   ██║██████╔╝██║     █████╗     ██║██║   ██║
+ ██║  ██║██╔══██╗██╔══██║██║   ██║██║   ██║██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██║     ██╔══╝     ██║██║   ██║
+ ██████╔╝██║  ██║██║  ██║╚██████╔╝╚██████╔╝██║ ╚████║██║     ╚██████╔╝██║  ██║╚██████╗███████╗██╗██║╚██████╔╝
+ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝╚═╝╚═╝ ╚═════╝ 
+                                                                                                             
+{Fore.WHITE}═══════════════════════════════════════════════════════════════════════════════════════════════════════════════
+{Style.BRIGHT + Fore.YELLOW}  
+                                                 Coded by Eagle Eye
+                                              Web Directory Bruteforcer
+                                    https://dragonforce.io | Telegram: dragonforceio
+                                   Get Started With (pip install -r requirements.txt)                                                                                            
+
+{Fore.WHITE}═══════════════════════════════════════════════════════════════════════════════════════════════════════════════
+""")
+def helpdesk():
+    print("Usage : python dirbrute.py -u http://example.com/ -w wordlist.txt")
+
+def checkUrl(url):
+    web_arr = urlparse(url)
+    if(web_arr.scheme=="" and web_arr.path==url):
+        return "http://" + url + "/"
+    elif(web_arr.path=="/"):
+        return url
     else:
-        print("Unknown command '{}' , -h/--help for Help ".format(sys.argv[2]))
+        return url + "/"
 
+def loadTxt(word):
+    try:
+        f = open(word,'r')
+        return f.readlines()
+    except FileNotFoundError:
+        print("File you request doesn't exist!")
+
+def connectURL(url):
+    scraper = cloudscraper.create_scraper(
+    browser = {
+        'browser' : 'chrome',
+        'platform' : 'darwin',
+        'mobile': False
+    }
+)
+    try:
+        try:
+            conn = scraper.get(url,timeout=3)
+            print("{} -> {}".format(conn.status_code,url))
+        except KeyboardInterrupt:
+            print("Process stopped!")
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
+    except:
+        print("No connection! -> {}".format(url))
+        sys.exit(0)
+
+def startX(url,word):
+    load = loadTxt(word)
+    for line in load:
+        fullURL = url+line.replace("\n","")
+        connectURL(fullURL)
+
+def position(arr,types):
+    if(types=="-u" or types=="-w"):
+        return arr.index(types) + 1
+    else:
+        print("No such option for {}".format(types))
+
+def main():
+    start = time.perf_counter()
+
+    url = sys.argv[position(sys.argv,'-u')]
+    word = sys.argv[position(sys.argv,'-w')]
+    startX(url,word)
+
+if __name__ == '__main__':
+    main()
